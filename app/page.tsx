ebe4ef40735c6167dbd61b3701a353a5961272f0
Ebuzer Yalcin <ebuzeryalcin@Macbook-m1.home>
2022-12-15 21:09:31 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { categories } from "../constants";
+import fetchNews from "../utils/fetchNews";
+import NewsList from "./NewsList";
+
+async function Homepage() {
+  //fetch the news data
+
+  const news: NewsResponse =
+    await fetchNews(categories.join(","));
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ utils/fetchNews.ts
@@ -0,0 +1,79 @@
+import { gql } from "graphql-request";
+import sortNewsByImage from "./sortNewsByImage";
+
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean
+) => {
+  //GraphQL query
+  const query = gql`
+    query MyQuery(
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "us"
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
+        data {
+          author
+          category
+          country
+          description
+          image
+          language
+          published_at
+          source
+          title
+          url
+        }
+        pagination {
+          count
+          limit
+          offset
+          total
+        }
+      }
+    }
+  `;
+  //Fetch funcion with Next.js 13 caching...
+  const res = await fetch(
+    "https://mongagua.stepzen.net/api/lame-jackal/__graphql",
+    {
+      method: "POST",
+      cache: isDynamic ? "no-cache" : "default",
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 60 },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          access_key: process.env.MEDIASTACK_API_KEY,
+          categories: category,
+          keywords: keywords,
+        },
+      }),
+    }
+  );
+
+  console.log(
+    "LOADING NEW DATA FROM API for category >>> ",
+    category,
+    keywords
+  );
+
+  const newsResponse = await res.json();
+  //Sort
+  const news = sortNewsByImage(newsResponse.data.myQuery);
+
+  //return
+  return news;
+};
+export default fetchNews;
